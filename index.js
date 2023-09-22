baf44bcc95b58047327fd82707e40c37b2f049fd
Hadil Khenissa <hadil.khenissa2@gmail.com>
2023-09-22 23:44:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+//receivesAFunction
+function receivesAFunction(callback) {
+    // Call the callback function
+    callback();
+  }
+  
+  receivesAFunction(() => {
+    console.log("This is a callback function.");
+  });
+  
+  //returnsANamedFunction
+  function returnsANamedFunction() {
+    //  return a named function
+    return function namedFunction() {
+      console.log("This is a named function.");
+    };
+  }
+  
+  
+  const namedFunc = returnsANamedFunction();
+  namedFunc(); 
+
+  //returnsAnAnonymousFunction
+
+  function returnsAnAnonymousFunction() {
+  //  return an anonymous function
+  return () => {
+    console.log("This is an anonymous function.");
+  };
+}
+
+
+const anonymousFunc = returnsAnAnonymousFunction();
+anonymousFunc(); 
+
+  
